dcb641a87e6026e7d9f0cdc127817dae3ee8f797
mohit0268 <mohitpurohit020@gmail.com>
2026-03-24 09:59:23 +0530

    Feat : data processing completed in bulk in DB

--- src/services/fileServices.js
+++ src/services/fileServices.js
@@ -28,11 +28,14 @@ const formatData = (data) => {
 
   return data.map((row) => ({
     policyNumber: row["Policy Number"] || row.policy_number,
+    firstName : row.firstname,
     agent: row["Agent"] || row.agent,
     account: row["Account"] || row.account_name,
-    carrier: row["Carrier"] || row.carrier_name,
+    carrier: row["Carrier"] || row.company_name,
     lob: row["LOB"] || row.category_name,
     premium: row["Premium"] || row.premium_amount,
+    startDate: row.policy_start_date ? new Date(row.policy_start_date) : null,
+    endDate: row.policy_end_date ? new Date(row.policy_end_date) : null,
   }));
 };
 
--- src/workers/fileWorker.js
+++ src/workers/fileWorker.js
@@ -3,19 +3,112 @@ const xlsx = require("xlsx");
 const mongoose = require("mongoose");
 const { parseData, formatData } = require("../services/fileServices.js");
 
+const Policy = require("../models/policy");
+const Agent = require("../models/agent");
+const Account = require("../models/account");
+const Carrier = require("../models/carrier");
+const LOB = require("../models/LOB");
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(async () => {
     try {
-      const processedData =await parseData(workerData.filepath);
+      const processedData = await parseData(workerData.filepath);
       const formattedData = formatData(processedData);
-      console.log(formattedData[0]);
 
-      parentPort.postMessage({
-        message: "Worker executed successfully",
-        totalRows: formattedData.length,
-      });
+      //caching it for optimization
+      const agentSet = new Set();
+      const accountSet = new Set();
+      const carrierSet = new Set();
+      const lobSet = new Set();
 
+      for (const row of formattedData) {
+        if (row.agent) agentSet.add(row.agent);
+        if (row.account) accountSet.add(row.account);
+        if (row.carrier) carrierSet.add(row.carrier);
+        if (row.lob) lobSet.add(row.lob);
+      }
+
+      await Agent.bulkWrite(
+        [...agentSet].map((name) => ({
+          updateOne: {
+            filter: { name },
+            update: { $set: { name } },
+            upsert: true,
+          },
+        })),
+      );
+
+      await Account.bulkWrite(
+        [...accountSet].map((name) => ({
+          updateOne: {
+            filter: { name },
+            update: { $set: { name } },
+            upsert: true,
+          },
+        })),
+      );
+
+      await Carrier.bulkWrite(
+        [...carrierSet].map((name) => ({
+          updateOne: {
+            filter: { name: name || "Unknown" },
+            update: { $set: { name: name || "Unknown" } },
+            upsert: true,
+          },
+        })),
+      );
+
+      await LOB.bulkWrite(
+        [...lobSet].map((name) => ({
+          updateOne: {
+            filter: { name },
+            update: { $set: { name } },
+            upsert: true,
+          },
+        })),
+      );
+
+      const agents = await Agent.find();
+      const accounts = await Account.find();
+      const carriers = await Carrier.find();
+      const lobs = await LOB.find();
+
+      const agentMap = new Map(agents.map((a) => [a.name, a._id]));
+      const accountMap = new Map(accounts.map((a) => [a.name, a._id]));
+      const carrierMap = new Map(carriers.map((a) => [a.name, a._id]));
+      const lobMap = new Map(lobs.map((a) => [a.name, a._id]));
+
+      const policyOps = [];
+
+      for (const row of formattedData) {
+        if (!row.policyNumber || !row.agent) continue;
+
+        policyOps.push({
+          updateOne: {
+            filter: { policyNumber: row.policyNumber },
+            update: {
+              $setOnInsert: {
+                policyNumber: row.policyNumber,
+                premium: row.premium,
+                startDate: row.startDate,
+                endDate: row.endDate,
+                policyHolderName: row.firstName || "Unknown",
+
+                agent: agentMap.get(row.agent),
+                account: accountMap.get(row.account),
+                carrier: carrierMap.get(row.carrier || "Unknown"),
+                lob: lobMap.get(row.lob),
+              },
+            },
+            upsert: true,
+          },
+        });
+      }
+
+      await Policy.bulkWrite(policyOps);
+
+      parentPort.postMessage({ message: "Bulk processing completed" });
     } catch (error) {
       parentPort.postMessage({ error: error.message });
     }
